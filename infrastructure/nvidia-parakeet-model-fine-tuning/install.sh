e493e88d65c71c700c5a3267170a6be477158504
Iman Abbasnejad <imanaba@amazon.com>
2026-02-24 02:16:49 +0000

    modify the requirements and installation dependencies

--- infrastructure/nvidia-parakeet-model-fine-tuning/install.sh
+++ infrastructure/nvidia-parakeet-model-fine-tuning/install.sh
@@ -8,8 +8,9 @@ conda create -n $ENV_NAME python=$PYTHON_VERSION -y
 
 # Use conda run instead of conda activate in scripts
 echo "Installing packages in the conda environment: $ENV_NAME..."
-conda run -n $ENV_NAME pip install torch torchvision torchaudio
+conda run -n $ENV_NAME pip install torch==2.10.0 torchaudio==2.10.0 torchvision==0.25.0
 conda run -n $ENV_NAME pip install -r requirements.txt
+
 
 echo "Installing system dependencies..."
 sudo apt-get update 
@@ -19,7 +20,6 @@ sudo apt-get install -y \
     poppler-utils \
     build-essential \
     git \
-    awscli \
     gcc \
     g++ \
     make \
@@ -30,6 +30,8 @@ sudo apt-get install -y \
     sox
 
 # Install Cython (needed for NeMo)
-conda run -n $ENV_NAME pip install Cython
+conda run -n $ENV_NAME python -m pip install Cython
+conda run -n $ENV_NAME pip install --upgrade torch torchvision torchaudio numba
+
 
 echo "Setup complete! You can activate your environment using: conda activate $ENV_NAME"
